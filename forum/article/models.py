9aec4336c02fdd2f173ed7765155465ec8ddd929
YiFeng Qian <464428607@qq.com>
2017-12-15 15:28:24 +0800

    don't waste any time

--- forum/article/models.py
+++ forum/article/models.py
@@ -3,8 +3,8 @@ from block.models import Block
 
 class Article(models.Model):
     block = models.ForeignKey(Block,verbose_name="板块ID")
-    title = models.CharField("板块名称",max_length=100)
-    content = models.CharField("板块描述",max_length=10000)
+    title = models.CharField("标题",max_length=100)
+    content = models.CharField("作者",max_length=10000)
     status = models.IntegerField("状态",choices=((0,"正常"),(-1,"删除")))
 
 
--- forum/article/views.py
+++ forum/article/views.py
@@ -8,3 +8,9 @@ def article_list(request,block_id):
     block = Block.objects.get(id=block_id)
     articles_objs = Article.objects.filter(block=block,status=0).order_by("-id")
     return render(request,"article_list.html",{"articles":articles_objs,"b":block})
+
+def article_create(request,block_id):
+    block_id = int(block_id)
+    block = Block.objects.get(id=block_id)
+    articles_objs = Article.objects.filter(block=block,status=0).order_by("-id")
+    return render(request,"article_create.html",{"articles":articles_objs,"b":block})
