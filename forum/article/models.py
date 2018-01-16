a8249060706c81f0acceecbabd8e448b54f58624
YiFeng Qian <464428607@qq.com>
2018-01-16 23:14:38 +0800

    pagination

--- forum/article/models.py
+++ forum/article/models.py
@@ -4,7 +4,7 @@ from block.models import Block
 class Article(models.Model):
     block = models.ForeignKey(Block,verbose_name="板块ID")
     title = models.CharField("标题",max_length=100)
-    content = models.CharField("作者",max_length=10000)
+    content = models.CharField("内容",max_length=10000)
     status = models.IntegerField("状态",choices=((0,"正常"),(-1,"删除")))
 
 
--- forum/article/views.py
+++ forum/article/views.py
@@ -2,29 +2,67 @@ from django.shortcuts import redirect
 from django.shortcuts import render
 from block.models import Block
 from article.models import Article
+from article.forms import ArticleForm
+from django.views.generic import View,DetailView
+from django.core.paginator import Paginator
+
 
 
 def article_list(request,block_id):
     block_id = int(block_id)
     block = Block.objects.get(id=block_id)
-    articles_objs = Article.objects.filter(block=block,status=0).order_by("-id")
-    return render(request,"article_list.html",{"articles":articles_objs,"b":block})
+    all_articles = Article.objects.filter(block=block,status=0).order_by("-id")
+    ARTICLE_CNT_1PAGE = 1
+    page_no = int(request.GET.get("page_no","1"))
+    p = Paginator(all_articles,ARTICLE_CNT_1PAGE)
+    page = p.page(page_no)
+    articles_objs = page.object_list
+    if page_no > p.num_pages:
+        page_no = p.num_pages
+    if page_no <= 0:
+        page_no = 1
+    page_cnt = p.num_pages
+    page_links = [i for i in range(page_no - 3,page_no + 4)
+                  if i > 0 and i <= p.num_pages]
+    len(page_links) == page_cnt
+    previous_link = page_links[0]-1
+    next_link = page_links[-1]+1
+    has_previous = previous_link > 0
+    has_next = next_link <= page_cnt
+    return render(request,"article_list.html",{"articles":articles_objs,"b":block,"page_cnt":page_cnt,
+                                               "current_no":page_no,"page_links":page_links,
+                                               "previous_link":previous_link,"next_link":next_link,
+                                               "previous":has_previous,"next":has_next,"len":len(page_links)})
 
-def article_create(request,block_id):
-    block_id = int(block_id)
-    block = Block.objects.get(id=block_id)
-    if request.method == "GET":
-        return render(request,"article_create.html",{"b":block,})
-    else:
-        title = request.POST["title"].strip()
-        content = request.POST["content"].strip()
-        if not title or not content:
-            return render(request,"article_create.html",
-                          {"b":block,"error":"傻鸟，你啥都没有写！！！！"})
-        if len(title) > 100 or len(content) > 10000:
-            return render(request,"article_create.html",
-                          {"b":block,"error":"不是啥东西都越长越好的！！！",
-                          "title":title,"content":content})
-        article = Article(block=block,title=title,content=content,status=0)
-        article.save()
-    return redirect("/article/list/%s" % block_id)
+
+
+class ArticleCreateView(View):
+
+    template_name = "article_create.html"
+
+    def init_data(self,block_id):
+        self.block_id = block_id
+        self.block = Block.objects.get(id=block_id)
+
+    def get(self,request,block_id):
+        self.init_data(block_id)
+        return render(request,self.template_name,{"b":self.block})
+
+    def post(self,request,block_id):
+        self.init_data(block_id)
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.block = self.block
+            article.status = 0
+            article.save()
+            return redirect("/article/list/%s" % self.block_id)
+        else:
+            return render(request,self.template_name,{"b":self.block,"form":form})
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+    context_object_name = "a"
+
